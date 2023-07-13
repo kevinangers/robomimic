e2078552af2fc15ea83abdb2f5fa646567c549d0
snasiriany <snasiriany@gmail.com>
2023-07-12 23:54:31 -0500

    WIP: action normalization

--- robomimic/algo/algo.py
+++ robomimic/algo/algo.py
@@ -225,7 +225,7 @@ class Algo(object):
             if k in batch and batch[k] is not None:
                 batch[k] = ObsUtils.process_obs_dict(batch[k])
                 if obs_normalization_stats is not None:
-                    batch[k] = ObsUtils.normalize_obs(batch[k], obs_normalization_stats=obs_normalization_stats)
+                    batch[k] = ObsUtils.normalize_dict(batch[k], obs_normalization_stats=obs_normalization_stats)
         return batch
 
     def train_on_batch(self, batch, epoch, validate=False):
@@ -477,6 +477,8 @@ class RolloutPolicy(object):
             ob (dict): single observation dictionary from environment (no batch dimension, 
                 and np.array values for each key)
         """
+        if self.obs_normalization_stats is not None:
+            ob = ObsUtils.normalize_dict(ob, obs_normalization_stats=self.obs_normalization_stats)
         ob = TensorUtils.to_tensor(ob)
         ob = TensorUtils.to_batch(ob)
         ob = TensorUtils.to_device(ob, self.policy.device)
